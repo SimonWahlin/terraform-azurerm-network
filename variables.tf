add05a6bf1cbf44a0515e65e8508768a81fe61f4
Simon Wåhlin <simon.wahlin@gmail.com>
2019-11-03 14:02:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,22 +14,26 @@ variable "location" {
 
 variable "address_space" {
   description = "The address space that is used by the virtual network."
+  type        = "string"
   default     = "10.0.0.0/16"
 }
 
 # If no values specified, this defaults to Azure DNS 
 variable "dns_servers" {
   description = "The DNS servers to be used with vNet."
+  type        = "list"
   default     = []
 }
 
 variable "subnet_prefixes" {
   description = "The address prefix to use for the subnet."
+  type        = "list"
   default     = ["10.0.1.0/24"]
 }
 
 variable "subnet_names" {
   description = "A list of public subnets inside the vNet."
+  type        = "list"
   default     = ["subnet1"]
 }
 
